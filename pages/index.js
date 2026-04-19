98713cfa20c555419d9138008d75ab588982d63b
RaminEjlal <ramin.edjlal1359@gmail.com>
2026-04-19 20:40:51 +0330

    Update

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <main style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <h1 style={{fontSize: '2rem', fontWeight: 'bold'}}>بله بات آماده است!</h1>
+    </main>
+  );
+}
